43f42fccb1c0cee2c9911f72a530f5e6d2e4dce0
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-12-14 19:48:46 -0300

    feat: decrease size of getbyidusuario in chat

--- sid/src/main/java/br/sc/weg/sid/repository/MensagemRepository.java
+++ sid/src/main/java/br/sc/weg/sid/repository/MensagemRepository.java
@@ -1,10 +1,16 @@
 package br.sc.weg.sid.repository;
 
+import br.sc.weg.sid.model.entities.Chat;
 import br.sc.weg.sid.model.entities.Mensagem;
+import br.sc.weg.sid.model.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MensagemRepository extends JpaRepository<Mensagem, Integer> {
 
+    List<Mensagem> findByIdChatAndIdUsuario(Chat idChat, Usuario idUsuario);
 }
