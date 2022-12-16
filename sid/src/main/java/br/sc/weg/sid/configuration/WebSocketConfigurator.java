ed565669fa8f0f5ac98c5874789a3eaf192316ac
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-12-16 13:27:58 -0300

    fix: try to resolve the error

--- sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
+++ sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfigurator implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
