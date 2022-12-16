4239cb9675ebad9a80803b66747b37afd8f4c380
HenriqueCole <henriquecolefernandes@gmail.com>
2022-12-16 13:40:33 -0300

    refactor: removed setAllowedOrigins

--- sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
+++ sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
@@ -2,16 +2,18 @@ package br.sc.weg.sid.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@CrossOrigin
 @EnableWebSocketMessageBroker
 public class WebSocketConfigurator implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
