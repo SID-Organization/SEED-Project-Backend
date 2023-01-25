c84142d3208c301198c5ac8c2db75f906478ec6a
rebelattogustavo <gu.zapella@gmail.com>
2023-01-25 16:12:31 -0300

    fix: fixed the bug for workflow historic

--- sid/src/main/java/br/sc/weg/sid/controller/HistoricoWorkflowController.java
+++ sid/src/main/java/br/sc/weg/sid/controller/HistoricoWorkflowController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
