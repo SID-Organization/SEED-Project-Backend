87c588021e364fb847029126477bb5c4e4fcd781
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-03-14 14:20:51 -0300

    feat: auth filter pronto

--- sid/src/main/java/br/sc/weg/sid/auth/filters/AuthFilter.java
+++ sid/src/main/java/br/sc/weg/sid/auth/filters/AuthFilter.java
@@ -3,6 +3,9 @@ package br.sc.weg.sid.auth.filters;
 import antlr.Token;
 import br.sc.weg.sid.security.service.JpaService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -16,13 +19,31 @@ public class AuthFilter extends OncePerRequestFilter {
 
 //    private TokenUtils tokenUtils;
 
-    private JpaService jpaService;
+//    private JpaService jpaService;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (request.getRequestURI().equals("/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        try {
+            String token = tokenUtils.buscarCookie(request);
+            Boolean valido = tokenUtils.validaToken(token);
+            if (valido) {
+                System.out.println("Token válido!");
+                Long usuarioCPF = tokenUtils.getUsuarioCPF(token);
+                UserDetails usuario = jpaService.loadUserByCPF(usuarioCPF);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario.getUsername(), null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido");
         }
     }
 }
