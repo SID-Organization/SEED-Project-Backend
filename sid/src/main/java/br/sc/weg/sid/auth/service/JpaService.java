319eaa6048aaad5c8f71f3447703afdfea5bf954
rebelattogustavo <gu.zapella@gmail.com>
2023-03-14 14:06:28 -0300

    feat: Added the JpaService class

--- sid/src/main/java/br/sc/weg/sid/auth/service/JpaService.java
+++ sid/src/main/java/br/sc/weg/sid/auth/service/JpaService.java
@@ -1,8 +1,34 @@
 package br.sc.weg.sid.auth.service;
 
+import br.sc.weg.sid.auth.users.UserJpa;
+import br.sc.weg.sid.model.entities.Usuario;
+import br.sc.weg.sid.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
-public class JpaService {
+import java.util.Optional;
 
+@Service
+public class JpaService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String numeroDeCadastroString) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional;
+        try{
+            Integer numeroDeCadastro = Integer.parseInt(numeroDeCadastroString);
+            usuarioOptional = usuarioRepository.findById(numeroDeCadastro);
+        }catch(Exception e){
+            throw new UsernameNotFoundException("Erro ao buscar usuário! " + e.getMessage());
+        }
+        if (usuarioOptional.isPresent()) {
+            return new UserJpa(usuarioOptional.get());
+        }
+        throw new UsernameNotFoundException("Usuário não encontrado!");
+    }
 }
