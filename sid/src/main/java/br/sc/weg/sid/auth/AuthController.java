c14943a99ff1203206c8961c89416423231c9cd6
LEONARDO GIUSEPPE DE SOUZA RAFAELLI <leonardo_rafaelli@estudante.sesisenai.org.br>
2023-03-14 14:27:25 -0300

    feat: token utils

---
+++ sid/src/main/java/br/sc/weg/sid/auth/AuthController.java
@@ -0,0 +1,11 @@
+package br.sc.weg.sid.auth;
+
+import br.sc.weg.sid.auth.service.JpaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+
+}
---
+++ sid/src/main/java/br/sc/weg/sid/auth/utils/TokenUtils.java
@@ -0,0 +1,50 @@
+package br.sc.weg.sid.auth.utils;
+
+import br.sc.weg.sid.auth.users.UserJpa;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import io.jsonwebtoken.Jwts;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+
+public class TokenUtils {
+    private final String senhaForte = "c127a7b6adb013a5ff879ae71afa62afa4b4ceb72afaa54711dbcde67b6dc325";
+
+    public String gerarToken(Authentication authentication){
+        UserJpa userJpa = (UserJpa) authentication.getPrincipal();
+        return Jwts.builder()
+                .setIssuer("Seed Application")
+                .setSubject(userJpa.getUsuario().getNumeroCadastroUsuario().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 1800000))
+                .signWith(SignatureAlgorithm.HS256, senhaForte)
+                .compact();
+    }
+
+    public Boolean validarToken(String token){
+        try{
+            Jwts.parser().setSigningKey(senhaForte).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Integer getUsuarioNumeroCadastro(String token) {
+        return Integer.parseInt(Jwts.parser()
+                .setSigningKey(senhaForte)
+                .parseClaimsJws(token)
+                .getBody().getSubject());
+    }
+
+    public String buscarCookie(HttpServletRequest request, String nomeCookie){
+        Cookie cookie = WebUtils.getCookie(request, nomeCookie);
+        if (cookie != null) return cookie.getValue();
+        throw new RuntimeException("Cookie não encontrado");
+    }
+
+}
