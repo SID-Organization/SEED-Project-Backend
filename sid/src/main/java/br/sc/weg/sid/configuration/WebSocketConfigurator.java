6c07c0e889639122b5c8e0c29be12f3997d27cf5
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-05-18 14:45:06 -0300

    feat: apenas arrumando algumas coisas do websocket

--- sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
+++ sid/src/main/java/br/sc/weg/sid/configuration/WebSocketConfigurator.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @CrossOrigin
@@ -13,13 +14,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfigurator implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS()
+                //2gb
+                .setStreamBytesLimit(200000)
+                //1gb
+                .setHttpMessageCacheSize(10000)
+                //30 segundos
+                .setDisconnectDelay(30 * 10000);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/demanda");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        //2gb
+        registration.setMessageSizeLimit(2 * 1024 * 1024);
     }
 }
 
--- sid/src/main/java/br/sc/weg/sid/controller/MensagemController.java
+++ sid/src/main/java/br/sc/weg/sid/controller/MensagemController.java
@@ -5,6 +5,7 @@ import br.sc.weg.sid.model.entities.Chat;
 import br.sc.weg.sid.model.entities.Mensagem;
 import br.sc.weg.sid.model.service.ChatService;
 import br.sc.weg.sid.model.service.MensagemService;
+import com.google.gson.Gson;
 import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,12 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -34,23 +39,34 @@ public class MensagemController {
 
     /**
      * Esta funcao é um mapeamento de requisição WebSocket que recebe uma mensagem e a envia para o chat correspondente.
+     *
      * @param mensagemDTO - Parâmetro que representa o objeto MensagemDTO, que contém os dados da mensagem a ser enviada.
      * @return ResponseEntity<Object> - Retorna um objeto ResponseEntity com status 200 e o corpo contendo uma mensagem de sucesso.
      * @throws Exception - Retorna uma mensagem de erro caso não seja possível enviar a mensagem.
      */
     @MessageMapping("/sid/api/mensagem")
-    public ResponseEntity<Object> receiveMessage(@RequestBody MensagemDTO mensagemDTO){
+    public ResponseEntity<Object> receiveMessage(@RequestBody MensagemDTO mensagemDTO) {
+        System.out.println(mensagemDTO);
         Mensagem mensagem = new Mensagem();
         BeanUtils.copyProperties(mensagemDTO, mensagem);
+        String values = mensagemDTO.getArquivoMensagem().toString();
+        Gson gson = new Gson();
+        HashMap map = gson.fromJson(values, HashMap.class);
+        byte[] arquivo = new byte[map.size()];
+
+        for (int i = 0; i < map.size(); i++) {
+            arquivo[i] = ((Double) map.get(String.valueOf(i))).byteValue();
+        }
+        mensagem.setArquivoMensagem(arquivo);
         // /demanda/{idDemanda}/{idChat}
         Chat chat = chatService.findById(mensagemDTO.getIdChat().getIdChat()).get();
         simpMessagingTemplate.convertAndSendToUser( /*id da demanda*/ chat.getIdDemanda().getIdDemanda().toString(), /*id do chat*/ mensagem.getIdChat().getIdChat().toString(), mensagem);
         try {
+            System.out.println(mensagem);
             mensagemService.save(mensagem);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Mensagem não salva! Message: " + e.getMessage());
         }
         return ResponseEntity.ok().body("Mensagem enviada com sucesso!");
     }
-
 }
