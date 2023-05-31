879b6b26287f376030cd6e9cf3cb0d9ec7a1d550
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2023-05-31 15:57:33 -0300

    feat: deletando a pasta sid e colocando os arquivos na pasta principal

---
+++ src/main/java/br/sc/weg/sid/repository/DemandaRepository.java
@@ -0,0 +1,34 @@
+package br.sc.weg.sid.repository;
+
+import br.sc.weg.sid.model.entities.*;
+import br.sc.weg.sid.model.enums.StatusDemanda;
+import br.sc.weg.sid.model.enums.TamanhoDemanda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Repository
+public interface DemandaRepository extends JpaRepository<Demanda, Integer> {
+
+    List<Demanda> findByTamanhoDemanda(TamanhoDemanda tamanhoDemanda);
+    List<Demanda> findByStatusDemanda(StatusDemanda statusDemanda);
+    List<Demanda> findByScoreDemanda(Double scoreDemanda);
+    List<Demanda> findBySolicitanteDemanda(Usuario solicitanteDemanda);
+    List<Demanda> findByTituloDemanda(String tituloDemanda);
+    List<Demanda> findByAnalistaResponsavelDemanda(Usuario analistaResponsavelDemanda);
+    List<Demanda> findByGerenteDaAreaDemanda(Usuario gerenteDaAreaDemanda);
+    List<Demanda> findByGestorResponsavelDemanda(Usuario gestorResponsavelDemanda);
+    List<Demanda> findByStatusDemandaAndSolicitanteDemanda(StatusDemanda statusDemanda, Usuario solicitanteDemanda);
+    List<Demanda> findRascunhosBySolicitanteDemanda(Usuario solicitanteDemanda);
+
+    List<Demanda> findAllByOrderByPrazoElaboracaoDemandaAsc();
+    List<Demanda> findAllByOrderByPrazoElaboracaoDemandaDesc();
+
+    List<Demanda> findAllByOrderByScoreDemandaAsc();
+    List<Demanda> findAllByOrderByScoreDemandaDesc();
+
+    List<Demanda> findAllByStatusDemandaIsAndSolicitanteDemandaNumeroCadastroUsuarioIs(StatusDemanda statusDemanda, Integer solicitanteDemanda_numeroCadastroUsuario);
+}
+
